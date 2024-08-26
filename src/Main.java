bf6bad903d59bb34f0541bf456b5425e4441a70e
Cr1staix <sayhell042@gmail.com>
2024-08-26 23:44:35 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int catAge = 1;
+int catWeight = 2;
+String catColor = "Black";
+String catName = "Pua";
+        System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + catWeight );
+    }
+}
