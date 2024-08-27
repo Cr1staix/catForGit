d0de827889cfb6dde1115b3ee2bad2ba2f90eca2
maxdd <maxizi1983@gmail.com>
2024-08-27 22:27:40 +0500

    catWeights commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-
 public class Main {
     public static void main(String[] args) {
-int catAge;
-int catWeight;
-String catColor;
-String catName;
-        System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + catWeight );
+        int catAge
+        int[] catWeights = {4, 5, 6, 3, 4};
+        String catColor
+        String catName
+
+        for (int weight : catWeights) {
+            System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + weight + " kg\n");
+        }
     }
 }
