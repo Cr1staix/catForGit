24700b2dd2ce499cacb3b7774bb55cfbe8a35ef5
Александр Шестаков <dddd30065@gmail.com>
2024-08-27 21:15:24 +0300

    commit: Mark+red

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int catAge;
         int catWeights = 5;
-        String catColor;
-        String catName;
+        String catColor = "red";
+        String catName = "Mark";
 
 
         System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + catWeights + " kg\n");
