964bc1e145e2630deb923baf4850c103a593fd2e
Cr1staix <sayhell042@gmail.com>
2024-08-28 00:45:28 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int catAge
-        int[] catWeights = {4, 5, 6, 3, 4};
-        String catColor
-        String catName
+        int catAge;
+        int catWeights = 5;
+        String catColor;
+        String catName;
 
-        for (int weight : catWeights) {
-            System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + weight + " kg\n");
-        }
+
+        System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + catWeights + " kg\n");
+
     }
 }
