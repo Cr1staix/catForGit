f25e6013d02e6c7c233b9c86146c8fcaed79372f
Cr1staix <sayhell042@gmail.com>
2024-08-29 01:12:55 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< feature-age
-        int catAge = 5 ;
+        int catAge = 5;
         int catWeights = 5;
         String catColor = "red";
-        String catName = "Mark";
-=======
-        int catAge = 3;
-        int catWeights = 5;
-        String catColor;
         String catName = "Jack";
->>>>>>> master
-
-
         System.out.println("Cat name - " + catName + "\nCat age - " + catAge + "\nCat color - " + catColor + "\nCat weight - " + catWeights + " kg\n");
-
     }
 }
